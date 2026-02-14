2c7228095348b9af9e6c78d69fa0954756ae43f7
webzyro <gmbworkswebzyro@gmail.com>
2026-02-14 17:59:32 +0530

    fixed the suggested coderabbitai bug

--- app/Models/CityPage.php
+++ app/Models/CityPage.php
@@ -24,17 +24,7 @@ class CityPage extends Model
         'is_active' => 'boolean',
     ];
 
-    /**
-     * Set the city page's description with sanitization.
-     *
-     * @param string $value
-     * @return void
-     */
-    public function setDescriptionAttribute($value)
-    {
-        $allowedTags = '<h1><h2><h3><h4><h5><h6><p><ul><li><ol><a><b><i><u><strong><em><br><span><div><img>';
-        $this->attributes['description'] = strip_tags($value, $allowedTags);
-    }
+
 
     public function scopeActive($query)
     {
